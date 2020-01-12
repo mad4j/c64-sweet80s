24c4a1fcc166501d1a397c66518e5c9a95831f7f
Daniele Olmisani <mad4j@users.noreply.github.com>
2020-01-12 21:44:42 +0100

    update

--- sweet80s.c
+++ sweet80s.c
@@ -12,14 +12,10 @@
 #include <c64.h>
 #include <cbm.h>
 #include <errno.h>
-#include <peekpoke.h>
 
 #include <string.h>
 #include <stdint.h>
 #include <stdbool.h>
-
-#include <stdio.h>
-
 
 
 /* KOALA image structure */
@@ -33,11 +29,10 @@
 #define BKGCOL_FILE_LENGTH        1
 
 /* */
-#define BITMAP_ADDRESS       0x2000
-#define SCREEN_ADDRESS       0x0400
-#define COLMAP_ADDRESS       0xd800
-#define BRDCOL_ADDRESS       0xd020
-#define BKGCOL_ADDRESS       0xd021
+#define BITMAP_ADDRESS       ((void*)0x2000)
+#define SCREEN_ADDRESS       ((void*)0x0400)
+#define COLMAP_ADDRESS       ((void*)0xD800)
+
 
 
 #define DEVICE_NUM                8
@@ -49,13 +44,22 @@ const char* FILES[] = { "ylenia", "marta", (const char*)0 };
 void initGraphics()
 {
     /* enable bitmap mode */
-    POKE(0xd011, 0x3b); 
+    VIC.ctrl1 = 0x3B;
 
-    /* enable multicolor graphic-mode */
-    POKE(0xd016, 0x18); 
+    /* enable multicolor graphic-mode */ 
+    VIC.ctrl2 = 0x18;
 
     /* set screen at $0400 and bitmap at $2000 */
-    POKE(0xd018, 0x1f);
+    VIC.addr = 0x1F;
+
+    /* set black background */
+    VIC.bgcolor0 = COLOR_BLACK;
+
+    /* set black border */ 
+    VIC.bordercolor = COLOR_BLACK;
+
+    /* clear bitmap area */
+    memset(BITMAP_ADDRESS, 0x00, BITMAP_FILE_LENGHT);
 }
 
 
@@ -71,19 +75,19 @@ uint32_t loadKOA(const char* fileName, uint8_t device)
 void renderKOA(uint8_t* imageBuffer) 
 {
     /* load bitmap data */
-    memcpy((void*)BITMAP_ADDRESS, (void*)(imageBuffer+BITMAP_FILE_OFFSET), BITMAP_FILE_LENGHT);
+    memcpy(BITMAP_ADDRESS, imageBuffer+BITMAP_FILE_OFFSET, BITMAP_FILE_LENGHT);
 
     /* load screen data */
-    memcpy((void*)SCREEN_ADDRESS, (void*)(imageBuffer+SCREEN_FILE_OFFSET), SCREEN_FILE_LENGTH);
+    memcpy(SCREEN_ADDRESS, imageBuffer+SCREEN_FILE_OFFSET, SCREEN_FILE_LENGTH);
 
     /* load color map */
-    memcpy((void*)COLMAP_ADDRESS, (void*)(imageBuffer+COLMAP_FILE_OFFSET), COLMAP_FILE_LENGTH);
+    memcpy(COLMAP_ADDRESS, imageBuffer+COLMAP_FILE_OFFSET, COLMAP_FILE_LENGTH);
 
     /* load background colour */
-    POKE(BKGCOL_ADDRESS, imageBuffer[BKGCOL_FILE_OFFSET]);
+    VIC.bgcolor0 = imageBuffer[BKGCOL_FILE_OFFSET];
 
     /* set black border */ 
-    POKE(BRDCOL_ADDRESS, 0x00);
+    VIC.bordercolor = COLOR_BLACK;
 }
 
 
